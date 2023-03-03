0142d7f9415291b0431f1051aae54284c7a44076
Muhammad Haseeb <mhussy02@gmail.com>
2023-03-03 16:12:59 +0500

    MEtamaskinit

---
+++ App.tsx
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { scheme } from './app.json';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  useWalletConnect,
+  withWalletConnect,
+} from '@walletconnect/react-native-dapp';
+const shortenAddress = (address: string) => {
+  return `${address.slice(0, 6)}...${address.slice(
+    address.length - 4,
+    address.length,
+  )}`;
+};
+function App() {
+  const connector = useWalletConnect();
+
+  const connectWallet = React.useCallback(() => {
+    return connector.connect();
+  }, [connector]);
+
+  const killSession = React.useCallback(() => {
+    return connector.killSession();
+  }, [connector]);
+  console.log(connector);
+  
+  return (
+    <View style={styles.sectionContainer}>
+      <TouchableOpacity onPress={connectWallet}>
+        <Text>Connect</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "center"
+  },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+});
+
+// export default App;
+
+
+export default withWalletConnect(App, {
+  clientMeta: {
+    description: 'Connect with WalletConnect',
+  },
+  redirectUrl: Platform.OS === 'web' ? window.location.origin : `${scheme}://`,
+  storageOptions: {
+    asyncStorage: AsyncStorage,
+  },
+});
